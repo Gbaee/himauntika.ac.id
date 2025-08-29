6e79025fcb95559c8b9ed492f5f6f0e3f609a7b7
Gbaee <gussbeada_7>
2025-08-29 18:37:24 +0700

    memperbarui

--- pages/himauntika.jsx
+++ pages/himauntika.jsx
@@ -405,11 +405,14 @@ export default function HimauntikaPage() {
         </div>
       </section>
 
-       {/* Projekan HIMAUNTIKA */}
-       <section id="projekan" className="py-16 md:py-20 px-4 md:px-6 bg-black/40">
+            {/* Projekan HIMAUNTIKA */}
+      <section
+        id="projekan"
+        className="py-16 md:py-20 px-4 md:px-6 bg-black/40"
+      >
         <div className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-10 items-center">
           {/* Gambar di kiri */}
-          <div className="flex justify-center">
+          <div className="flex justify-center order-1 md:order-none">
             <img
               src="/images/projekan.jpg" // ganti dengan foto showcase projekan
               alt="Projekan IoT & Robotic"
@@ -435,10 +438,7 @@ export default function HimauntikaPage() {
               . Kami sering membuat projek mulai dari perangkat cerdas sederhana
               hingga sistem otomatisasi yang bermanfaat. Tujuannya bukan hanya
               untuk mengasah keterampilan teknis, tetapi juga membangun budaya
-              kolaborasi, kreativitas, dan inovasi.  
-              <br />
-              <br />
-              Di website ini, kami akan menampilkan dokumentasi foto-foto
+              kolaborasi, kreativitas, dan inovasi. Di website ini, kami akan menampilkan dokumentasi foto-foto
               pengerjaan projek tersebut agar seluruh mahasiswa bisa melihat dan
               terinspirasi untuk ikut berkontribusi.
             </p>
