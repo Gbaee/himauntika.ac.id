8612e106972788199eff2242e2cc850fcc9cec57
Gbaee <guss4da@gmail.com>
2025-09-08 01:25:56 +0700

    tes

--- components/ui/TimelineWrapper.jsx
+++ components/ui/TimelineWrapper.jsx
@@ -1,30 +1,23 @@
 const TimelineWrapper = ({ children }) => {
   return (
     <div className="relative py-12 md:py-16">
-      {/* Garis vertikal animasi */}
-      <motion.div
-        initial={{ scaleY: 0 }}
-        animate={{ scaleY: 1 }}
-        transition={{ duration: 0.8 }}
-        className="absolute h-full w-1 bg-purple-700 origin-top
-left-4 md:left-1/2 md:transform md:-translate-x-1/2"
-      ></motion.div>
-
-      {/* Icon centang biru full bulatan */}
+      {" "}
       <div
-        className="absolute rounded-full overflow-hidden w-10 h-10 shadow-lg z-10
-left-4 -top-5 md:left-1/2 md:-translate-x-1/2 animate-bounce"
+        className="absolute h-full w-1 bg-purple-700
+left-4 md:left-1/2 md:transform md:-translate-x-1/2"
+      ></div>
+      {/* Icon centang biru */}
+      <div
+        className="absolute bg-purple-600 rounded-full w-8 h-8 flex items-center justify-center shadow-lg z-10
+left-4 -top-4 md:left-1/2 md:-translate-x-1/2 animate-bounce"
       >
-        {/* Gambar langsung memenuhi bulatan */}
         <img
           src="/images/centang biru.jpg"
           alt="check"
-          className="w-full h-full object-cover animate-pulse"
+          className="w-4 h-4 animate-pulse"
         />
       </div>
-
-      {/* Tambahin padding-left supaya isi section tidak dempet dengan garis */}
-      <div className="pl-12 md:pl-0">{children}</div>
+      {children}
     </div>
   );
 };
