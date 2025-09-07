77d4e63b4be1d2c06519c8dda8aafd06dc740cd2
Gbaee <guss4da@gmail.com>
2025-09-07 23:16:58 +0700

    mengganti code dibagian timeline

--- components/ui/TimelineWrapper.jsx
+++ components/ui/TimelineWrapper.jsx
@@ -1,11 +1,12 @@
 const TimelineWrapper = ({ children }) => {
   return (
     <div className="relative py-12 md:py-16">
-      {" "}
+      {/* Garis vertikal */}
       <div
         className="absolute h-full w-1 bg-purple-700
 left-4 md:left-1/2 md:transform md:-translate-x-1/2"
       ></div>
+
       {/* Icon centang biru */}
       <div
         className="absolute bg-purple-600 rounded-full w-8 h-8 flex items-center justify-center shadow-lg z-10
@@ -17,7 +18,9 @@ left-4 -top-4 md:left-1/2 md:-translate-x-1/2 animate-bounce"
           className="w-4 h-4 animate-pulse"
         />
       </div>
-      {children}
+
+      {/* Tambahin padding-left supaya isi section tidak dempet dengan garis */}
+      <div className="pl-12 md:pl-0">{children}</div>
     </div>
   );
 };
