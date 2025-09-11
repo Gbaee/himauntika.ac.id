9da6d8702c43d712abee0906d595aa0e2fa9c685
Gbaee <guss4da@gmail.com>
2025-09-11 14:00:38 +0700

    menambahakn centang biru di card kiri

--- components/ui/TipeOrangSection.jsx
+++ components/ui/TipeOrangSection.jsx
@@ -14,11 +14,31 @@ export default function TipeOrangSection() {
 
       {/* Grid Card */}
       <div className="grid md:grid-cols-2 gap-8 max-w-5xl w-full">
-        {/* Card 1 */}
+        {/* Card 1 dengan custom badge */}
         <motion.div
           whileHover={{ scale: 1.05 }}
           className="relative rounded-xl overflow-hidden border border-cyan-500/50 shadow-lg"
         >
+          {/* ✅ Custom SVG Badge */}
+          <div className="absolute -top-8 left-1/2 -translate-x-1/2 z-20">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="48"
+              height="48"
+              viewBox="0 0 24 24"
+              fill="none"
+            >
+              <path
+                fill="#1DA1F2"
+                d="M22.25 12c0-1.1.9-2 2-2h.75c1.1 0 2-.9 2-2v-.75c0-1.1-.9-2-2-2h-.75c-1.1 0-2-.9-2-2V2.5c0-1.1-.9-2-2-2h-.75c-1.1 0-2 .9-2 2v.75c0 1.1-.9 2-2 2h-.75c-1.1 0-2 .9-2 2V8c0 1.1.9 2 2 2h.75c1.1 0 2 .9 2 2v.75c0 1.1.9 2 2 2h.75c1.1 0 2-.9 2-2V12z"
+              />
+              <path
+                fill="#fff"
+                d="M10.5 13.5l-2-2L7 13l3.5 3.5L17 10.5l-1.5-1.5-5 5z"
+              />
+            </svg>
+          </div>
+
           <Image
             src="/images/orang-melompat.jpg" // ganti sesuai gambarmu
             alt="Orang"
@@ -26,6 +46,7 @@ export default function TipeOrangSection() {
             height={600}
             className="w-full h-[400px] object-cover"
           />
+
           <div className="absolute inset-0 bg-gradient-to-t from-black/70 via-transparent to-black/30 flex items-end justify-center p-6">
             <p className="text-lg font-semibold">
               Orang Yang Mau Melompat <br /> Untuk Merubah Hidupnya
@@ -33,7 +54,7 @@ export default function TipeOrangSection() {
           </div>
         </motion.div>
 
-        {/* Card 2 */}
+        {/* Card 2 biasa */}
         <motion.div
           whileHover={{ scale: 1.05 }}
           className="relative rounded-xl overflow-hidden border border-cyan-500/50 shadow-lg"
